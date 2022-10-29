e97be58dd4218599e9f0bd3c0d5ee1b3e9e73b38
Nikita <nikon15151@yandex.ru>
2022-10-30 02:13:54 +0300

    fix: transactional annotation added

--- src/main/java/ru/nikita/spring/ShopSpringApp/services/CategoriesService.java
+++ src/main/java/ru/nikita/spring/ShopSpringApp/services/CategoriesService.java
@@ -2,12 +2,14 @@ package ru.nikita.spring.ShopSpringApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.nikita.spring.ShopSpringApp.models.Category;
 import ru.nikita.spring.ShopSpringApp.repositories.CategoriesRepository;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class CategoriesService {
 
     private final CategoriesRepository categoriesRepository;
@@ -29,10 +31,12 @@ public class CategoriesService {
         return categoriesRepository.findById(id).orElse(null);
     }
 
+    @Transactional
     public void save(Category category) {
         categoriesRepository.save(category);
     }
 
+    @Transactional
     public void deleteById(int id) {
         categoriesRepository.deleteById(id);
     }
