d87c723fb4bfd29c36f47543684bd5235f10052e
Nikita <nikon15151@yandex.ru>
2022-10-30 12:52:19 +0300

    upd: methods' names, post content validation

--- src/main/java/ru/nikita/spring/ShopSpringApp/controllers/ItemController.java
+++ src/main/java/ru/nikita/spring/ShopSpringApp/controllers/ItemController.java
@@ -4,13 +4,18 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.nikita.spring.ShopSpringApp.dto.ItemDTO;
 import ru.nikita.spring.ShopSpringApp.models.Item;
 import ru.nikita.spring.ShopSpringApp.services.ItemsService;
+import ru.nikita.spring.ShopSpringApp.util.FieldErrorsData;
+import ru.nikita.spring.ShopSpringApp.util.ItemNotFoundException;
 import ru.nikita.spring.ShopSpringApp.util.ResponseData;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @RestController
@@ -26,7 +31,7 @@ public class ItemController {
     }
 
     @GetMapping
-    public ResponseEntity<ResponseData> getAllItems(
+    public ResponseEntity<ResponseData> findAllItems(
             @RequestParam(name = "category", required = false, defaultValue = "-1") int id) {
         List<Item> items;
         if (id > 0)
@@ -39,14 +44,27 @@ public class ItemController {
     }
 
     @GetMapping("/{id}")
-    public ItemDTO getItemById(@PathVariable("id") int id) {
+    public ItemDTO findItemById(@PathVariable("id") int id) {
+        Item foundItem = itemsService.findById(id);
+        if (Objects.isNull(foundItem)) throw new ItemNotFoundException();
         return convertToItemDTO(itemsService.findById(id));
     }
 
     @PostMapping
-    public ResponseEntity<ResponseData> addItem(@RequestBody ItemDTO itemDTO) {
+    public ResponseEntity<ResponseData> saveItem(
+            @RequestBody @Valid ItemDTO itemDTO,
+            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorsData = FieldErrorsData.getErrorsData(bindingResult.getFieldErrors());
+            return new ResponseEntity<>(new ResponseData(false, errorsData), HttpStatus.BAD_REQUEST);
+        }
         itemsService.save(convertToItem(itemDTO));
-        return new ResponseEntity<>(new ResponseData(true), HttpStatus.OK);
+        return new ResponseEntity<>(new ResponseData(true), HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<ResponseData> itemNotFoundExceptionHandler(ItemNotFoundException e) {
+        return new ResponseEntity<>(new ResponseData(false, "Такого товара нет"), HttpStatus.NOT_FOUND);
     }
 
     private ItemDTO convertToItemDTO (Item item) {
